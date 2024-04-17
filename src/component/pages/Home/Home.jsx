67cd5fa209704004ec99b79d2c0c7771829b7980
Prosantaroy1 <royprosanta172@gmail.com>
2024-04-17 19:08:30 +0100

    loading

--- src/component/pages/Home/Home.jsx
+++ src/component/pages/Home/Home.jsx
@@ -1,5 +1,16 @@
+import { useContext } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
 
 const Home = () => {
+    const {loading} = useContext(AuthContext);
+    if (loading) {
+        return <div className="flex justify-center items-center pt-40">
+            <span className="loading loading-bars loading-xs"></span>
+            <span className="loading loading-bars loading-sm"></span>
+            <span className="loading loading-bars loading-md"></span>
+            <span className="loading loading-bars loading-lg"></span>
+        </div>
+    }
     return (
         <div>
             <h3>This is pages Home section!</h3>
